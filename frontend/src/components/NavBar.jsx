d4f0a27e6e7d08991209fc1cfe5118928876a211
manotz1x <manoranjith5453@gmail.com>
2026-02-05 15:57:29 +0530

    3

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 export default function NavBar() {
   return (
     <div className="w-60 min-h-screen bg-gray-800 shadow p-4">
-      <h1 className="text-2xl font-bold text-blue-400 mb-6">
+      <h1 className="text-2xl font-bold text-green-400 mb-6">
         Money Manager
       </h1>
 
